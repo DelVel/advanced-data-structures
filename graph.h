a514f7c9b0adfc4cf7846add821395f1fd3cf84f
Taegyu Park <computerpark@postech.ac.kr>
2020-11-19 20:56:41 +0900

    Add graphs

--- graph.h
+++ graph.h
@@ -3,23 +3,108 @@
 
 #include <vector>
 #include <set>
-#include <stdexcept>
 
 using namespace std;
 
 class Graph
 {
+public:
+	virtual bool hasEdge(unsigned from, unsigned to) const = 0;
+
+	virtual size_t getNodes() const = 0;
+
+	virtual size_t getEdges() const = 0;
+
+	virtual ~Graph() {}
+};
+
+class ModifiableGraph : public Graph
+{
+public:
+	virtual bool addEdge(unsigned from, unsigned to) = 0;
+
+	virtual bool removeEdge(unsigned from, unsigned to) = 0;
+};
+
+class AdjacencyListDirectedGraph : public ModifiableGraph
+{
 private:
 	vector<set<unsigned>> _adjList;
+	const size_t _nodes;
 
 public:
-	Graph(size_t size) :_adjList(size, set<unsigned>()) {}
+	AdjacencyListDirectedGraph(size_t size) :_adjList(size, set<unsigned>()), _nodes(size) {}
 
-	bool hasEdge(const unsigned from, const unsigned to) const
+	bool hasEdge(unsigned from, unsigned to) const override
 	{
-		if (_adjList.size() <= from)
-			return false;
 		return _adjList.at(from).count(to);
+	}
+
+	bool addEdge(unsigned from, unsigned to) override
+	{
+		return _adjList.at(from).insert(to).second;
+	}
+
+	bool removeEdge(unsigned from, unsigned to) override
+	{
+		return _adjList.at(from).erase(to);
+	}
+
+	size_t getNodes() const override
+	{
+		return _nodes;
+	}
+
+	size_t getEdges() const override
+	{
+		size_t res = 0;
+		for (auto s : _adjList)
+			res += s.size();
+		return res;
+	}
+};
+
+class AdjacencyListUndirectedGraph : public ModifiableGraph
+{
+private:
+	vector<set<unsigned>> _adjList;
+	const size_t _nodes;
+
+public:
+	AdjacencyListUndirectedGraph(size_t size) :_adjList(size, set<unsigned>()), _nodes(size) {}
+
+	bool hasEdge(unsigned from, unsigned to) const override
+	{
+		if (from > to)
+			swap(from, to);
+		return _adjList.at(from).count(to);
+	}
+
+	bool addEdge(unsigned from, unsigned to) override
+	{
+		if (from > to)
+			swap(from, to);
+		return _adjList.at(from).insert(to).second;
+	}
+
+	bool removeEdge(unsigned from, unsigned to) override
+	{
+		if (from > to)
+			swap(from, to);
+		return _adjList.at(from).erase(to);
+	}
+
+	size_t getNodes() const override
+	{
+		return _nodes;
+	}
+
+	size_t getEdges() const override
+	{
+		size_t res = 0;
+		for (auto s : _adjList)
+			res += s.size();
+		return res;
 	}
 };
 
